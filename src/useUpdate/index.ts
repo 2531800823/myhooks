7d2fc33d991d40233a6a889208b647ba0d600eba
刘士朋 <2531800823@qq.com>
2023-10-16 22:15:24 +0800

    添加 useUpdate

---
+++ src/useUpdate/index.ts
@@ -0,0 +1,11 @@
+import { useCallback, useState } from "react";
+
+function useUpdate() {
+  const [, setState] = useState({});
+
+  return useCallback(() => {
+    setState({});
+  }, []);
+}
+
+export default useUpdate;
