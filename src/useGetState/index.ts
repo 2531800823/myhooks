00713feaba40cf2b1ed953a70d62d26f08470d3b
刘士朋 <2531800823@qq.com>
2023-10-21 22:29:06 +0800

    完成 useGetState

---
+++ src/useGetState/index.ts
@@ -0,0 +1,25 @@
+import { Dispatch, SetStateAction, useCallback, useRef, useState } from "react";
+
+type GetStateAction<S> = () => S;
+
+function useGetState<S>(
+  initialState: S | (() => S)
+): [S, Dispatch<SetStateAction<S>>, GetStateAction<S>];
+
+function useGetState<S = undefined>(): [
+  S | undefined,
+  Dispatch<SetStateAction<S | undefined>>,
+  GetStateAction<S | undefined>
+];
+
+function useGetState<S>(initialState?: S) {
+  const [state, setState] = useState(initialState);
+
+  const stateRef = useRef(state);
+
+  const getState = useCallback(() => stateRef.current, []);
+
+  return [state, setState, getState];
+}
+
+export default useGetState;
