283e0af9acf4613a9e2f4baeeefd6847368adf31
刘士朋 <2531800823@qq.com>
2023-10-21 20:59:18 +0800

    完成 useMap

---
+++ src/useMap/index.ts
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import useMemoizedFn from "@/useMemoizedFn";
+
+function useMap<K, T>(initialValue?: Iterable<readonly [K, T]>) {
+  const getInitValue = () => new Map(initialValue);
+  const [map, setMap] = useState(getInitValue);
+  const set = (key: K, entry: T) => {
+    setMap((prev) => {
+      const temp = new Map(prev);
+      temp.set(key, entry);
+      return temp;
+    });
+  };
+
+  const setAll = (newMap: Iterable<readonly [K, T]>) => {
+    setMap(new Map(newMap));
+  };
+
+  const remove = (key: K) => {
+    setMap((prev) => {
+      const temp = new Map(prev);
+      temp.delete(key);
+      return temp;
+    });
+  };
+
+  const reset = () => setMap(getInitValue());
+
+  const get = (key: K) => {
+    return map.get(key);
+  };
+
+  return [
+    map,
+    {
+      set: useMemoizedFn(set),
+      setAll: useMemoizedFn(setAll),
+      remove: useMemoizedFn(remove),
+      reset: useMemoizedFn(reset),
+      get: useMemoizedFn(get),
+    },
+  ];
+}
+
+export default useMap;
