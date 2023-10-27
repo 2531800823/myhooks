54b30940028120316d6a32e0a570b2f5e0bf4418
刘士朋 <2531800823@qq.com>
2023-10-27 22:52:40 +0800

    完成 useControllableValue

---
+++ src/useControllableValue/index.ts
@@ -0,0 +1,74 @@
+import { SetStateAction, useMemo, useRef } from "react";
+import useUpdate from "../useUpdate/index";
+import { isFunction } from "../utils/index";
+import useMemoizedFn from "@/useMemoizedFn";
+
+export interface Options<T> {
+  defaultValue?: T;
+  defaultValuePropName?: string;
+  valuePropName?: string;
+  trigger?: string;
+}
+
+export type Props = Record<string, any>;
+
+export interface StandardProps<T> {
+  value: T;
+  defaultValue?: T;
+  onChange: (val: T) => void;
+}
+
+function useControllableValue<T = any>(
+  props: StandardProps<T>
+): [T, (v: SetStateAction<T>) => void];
+function useControllableValue<T = any>(
+  props?: Props,
+  options?: Options<T>
+): [T, (v: SetStateAction<T>, ...args: any[]) => void];
+
+function useControllableValue<T = any>(
+  props: Props = {},
+  options: Options<T> = {}
+) {
+  const {
+    defaultValue,
+    defaultValuePropName = "defaultValue",
+    valuePropName = "value",
+    trigger = "onChange",
+  } = options;
+
+  const value = props[valuePropName] as T;
+  const isControlled = props.hasOwnProperty(valuePropName);
+
+  const initialValue = useMemo(() => {
+    if (isControlled) {
+      return value;
+    }
+    if (props.hasOwnProperty(defaultValuePropName)) {
+      return props[defaultValuePropName];
+    }
+    return defaultValue;
+  }, []);
+
+  const stateRef = useRef(initialValue);
+  if (isControlled) {
+    stateRef.current = value;
+  }
+
+  const update = useUpdate();
+
+  function setState(v: SetStateAction<T>, ...args: any[]) {
+    const r = isFunction(v) ? v(stateRef.current) : v;
+    if (!isControlled) {
+      stateRef.current = r;
+      update();
+    }
+    if (props[trigger]) {
+      props[trigger](r, ...args);
+    }
+  }
+
+  return [stateRef.current, useMemoizedFn(setState)] as const;
+}
+
+export default useControllableValue;
